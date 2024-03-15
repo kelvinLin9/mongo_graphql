faeefcdfd890bee20003dfe3ee8cb55ac04c1b61
kelvin <kelvin80121@hotmail.com>
2024-03-15 23:22:44 +0800

    Update MongoDB connection string

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 
 var app = express();
 
-mongoose.connect('mongodb+srv://kelvin80121:WBdXadSw3RjzpzHu@gt.ptvgaic.mongodb.net/?retryWrites=true&w=majority&appName=GT')
+mongoose.connect('mongodb+srv://kelvin80121:6pp17nWbIYfBlKTy@gt.ptvgaic.mongodb.net/?retryWrites=true&w=majority&appName=GT')
   .then(res=> console.log("連線資料成功"));
 
   app.use('/graphql', graphqlHTTP({
