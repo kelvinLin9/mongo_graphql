63ee4878b245f0bee99d05c0045303bb48f3902b
kelvin <kelvin80121@hotmail.com>
2024-03-19 23:16:18 +0800

    Update settings.json and app.js, refactor schema.js

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const { schema, root } = require('./graphql/schema');
+const schema = require('./graphql/schema'); // 確保路徑正確
 require('dotenv').config();
-
 const mongoose = require('mongoose');
 
 var app = express();
 
 mongoose.connect(process.env.DB_CONNECTION_STRING)
   .then(res => console.log("連線資料成功"))
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
-  app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true, // GraphQL IDE
-  }));
-
+app.use('/graphql', graphqlHTTP({
+  schema: schema, // 使用剛剛建立的 schema
+  graphiql: true, // 啟用 graphiql IDE
+}));
 
 module.exports = app;
+
+
+
+
+
+
+// const express = require('express');
+// const { graphqlHTTP } = require('express-graphql');
+// const { schema, root } = require('./graphql/schema');
+// require('dotenv').config();
+
+// const mongoose = require('mongoose');
+
+// var app = express();
+
+// mongoose.connect(process.env.DB_CONNECTION_STRING)
+//   .then(res => console.log("連線資料成功"))
+//   .catch(err => console.log(err))
+
+//   app.use('/graphql', graphqlHTTP({
+//     schema: schema,
+//     rootValue: root,
+//     graphiql: true, // GraphQL IDE
+//   }));
+
+
+// module.exports = app;
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,65 +1,81 @@
-const { buildSchema } = require('graphql');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/userModel');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLSchema,
+} = require('graphql');
+const Story = require('../models/storyModel');
+const Brick = require('../models/brickModel');
 
-let users = [];
+const PostType = new GraphQLObjectType({
+  name: 'Post',
+  fields: () => ({
+    id: { type: GraphQLID },
+    title: { type: GraphQLString },
+    content: { type: GraphQLString }, // 假設 Post 有標題和內容
+    image: { type: GraphQLString }, // 假設 Post 包含一個圖片鏈接
+  })
+});
 
-const schema = buildSchema(`
-type Query {
-  login(email: String!, password: String!): String
-  user(id: ID!): User
-}
+const BrickType = new GraphQLObjectType({
+  name: 'Brick',
+  fields: () => ({
+    id: { type: GraphQLID },
+    brickName: { type: GraphQLNonNull(GraphQLString) },
+    title: { type: GraphQLNonNull(GraphQLString) },
+    image: { type: GraphQLNonNull(GraphQLString) },
+    // 假設 PostType 已定義，這裡示範如何關聯 Post
+    content: {
+      type: new GraphQLList(PostType),
+      resolve(parent, args) {
+        // 根據 Brick 文檔中的 content ID 陣列查詢關聯的 Post 文檔
+        // 需要實現 PostType 和相應的 Mongoose 查詢
+      },
+    },
+  }),
+});
 
-  type Mutation {
-    register(email: String!, password: String!): User
+const StoryType = new GraphQLObjectType({
+  name: 'Story',
+  fields: () => ({
+    id: { type: GraphQLID },
+    storyName: { type: GraphQLNonNull(GraphQLString) },
+    title: { type: GraphQLString },
+    image: { type: GraphQLString },
+    content: {
+      type: new GraphQLList(BrickType),
+      resolve(parent, args) {
+        // 在這裡實現查詢透過 Story 文檔中的 content 關聯的 Brick 文檔
+        return Brick.find({_id: { $in: parent.content }});
+      },
+    },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    stories: {
+      type: new GraphQLList(StoryType),
+      args: {
+        storyName: { type: GraphQLString } // 允許查詢時使用 storyName 作為過濾條件
+      },
+      resolve(parent, args) {
+        // 如果提供了 storyName，則按照 storyName 過濾；否則，返回所有故事
+        if (args.storyName) {
+          return Story.find({ storyName: args.storyName });
+        } else {
+          return Story.find({});
+        }
+      }
+    },
+    // 其他查詢...
   }
+});
 
-  type User {
-    id: ID!
-    email: String!
-  }
-`);
 
-const root = {
-  login: async ({ email, password }) => {
-    const user = users.find(user => user.email === email);
-    if (!user) {
-      throw new Error('User not found');
-    }
-    const isValid = await bcrypt.compare(password, user.password);
-    if (!isValid) {
-      throw new Error('Invalid password');
-    }
-    return jwt.sign({ userId: user.id }, 'SECRET_KEY', { expiresIn: '1h' });
-  },
-  register: async ({ email, password }) => {
-    // 检查用户是否已存在
-    const existingUser = await User.findOne({ email: email });
-    if (existingUser) {
-      throw new Error('User already exists.');
-    }
-
-    // 加密密码
-    const hashedPassword = await bcrypt.hash(password, 12);
-
-    // 创建并保存新用户
-    const user = new User({
-      email,
-      password: hashedPassword
-    });
-    const result = await user.save();
-
-    // 返回新创建的用户信息
-    return { id: result.id, email: result.email };
-  },
-  user: ({ id }) => {
-    const user = users.find(user => user.id == id);
-    if (!user) {
-      throw new Error('User not found');
-    }
-    return { id: user.id, email: user.email };
-  },
-};
-
-module.exports = { schema, root };
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
